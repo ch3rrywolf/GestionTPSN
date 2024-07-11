b6efbe75d95a22229dad86776c64387725139f79
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-11 01:51:54 +0100

    Register Api , Relation User and Profile, authSchema.

--- helpers/authValidation_schema.js
+++ helpers/authValidation_schema.js
@@ -8,10 +8,5 @@ const authSchema = Joi.object({
     username: Joi.string().min(4).max(30).required(),
 });
 
-const loginSchema = Joi.object({
-    email: Joi.string().email().lowercase().required(),
-    password: Joi.string().min(4).required()
-});
-
 
 module.exports = { authSchema, loginSchema }
